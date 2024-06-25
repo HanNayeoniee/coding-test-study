a990a512c6a4d63accf1eab9adae6e824dc74f93
HanNayeoniee <nayeon2.han@gmail.com>
2024-06-25 09:09:20 +0900

    다른 풀이 추가

--- boj/2309.py
+++ boj/2309.py
@@ -51,3 +51,32 @@ def sol3():
                 print(num)
             # 여러 경우의 수가 나올 수 있으므로 종료
             break
+
+
+def sol4():
+    """
+    9명 중 제외할 2명 선택. combinations 사용하지 않고 구현
+    """
+    # 인덱스 a, b인 숫자 제외하고 출력
+    def print_ans(arr, a, b):
+        for i in range(9):
+            if i == a or i == b:
+                continue
+            print(arr[i])
+
+    # 입력 받기
+    heights = [int(input()) for _ in range(9)]
+    heights.sort()
+    total = sum(heights)
+
+    flag = False
+    for i in range(9):
+        if flag:
+            break
+        for j in range(i+1, 9):
+            # 9명 합 - 2명 키 == 100이 되는 경우 찾기
+            if total - heights[i] - heights[j] == 100:
+                # 2명 제외하고 출력
+                print_ans(heights, i, j)
+                flag = True
+                break
