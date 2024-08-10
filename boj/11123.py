bc5c1810e014b26253427dc424e17204e289dae3
HanNayeoniee <nayeon2.han@gmail.com>
2024-08-10 19:09:50 +0900

    DFS 풀이 추가

--- boj/11123.py
+++ boj/11123.py
@@ -4,6 +4,7 @@
 
 import sys
 
+### 풀이1 - BFS 탐색 사용
 def bfs(i, j):
     global cnt
     q = [(i, j)]
@@ -36,5 +37,41 @@ for _ in range(T):
         for j in range(W):
             if arr[i][j] == '#' and not visited[i][j]:
                 bfs(i, j)
+                cnt += 1
+    print(cnt)
+
+
+### 풀이2 - DFS 탐색 사용
+import sys
+sys.setrecursionlimit(10**6)
+
+T = int(sys.stdin.readline())
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def dfs(x, y):
+    arr[x][y] = '.'  # 방문한 위치는 풀로 표시
+    for k in range(4):
+        nx = x + dx[k]
+        ny = y + dy[k]
+        if nx<0 or nx>=H or ny<0 or ny>=W:
+            continue
+        if arr[nx][ny]=='#':
+            dfs(nx, ny)
+
+
+# 문제 입력받기
+for _ in range(T):
+    H, W = map(int, sys.stdin.readline().split())
+    arr = []
+    for h in range(H):
+        arr.append(list(sys.stdin.readline().strip()))
+
+    cnt = 0
+    # 양이 위치한 부분에서 탐색 시작
+    for i in range(H):
+        for j in range(W):
+            if arr[i][j] == '#':
+                dfs(i, j)
                 cnt += 1
     print(cnt)
